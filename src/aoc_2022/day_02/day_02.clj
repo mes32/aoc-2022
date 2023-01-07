980711f0a51e4d84caa19b1a65ea792f070790e5
Mike Stockman <stockman.mike@gmail.com>
2023-01-07 15:08:26 -0600

    solve day 2

--- src/aoc_2022/day_02/day_02.clj
+++ src/aoc_2022/day_02/day_02.clj
@@ -2,6 +2,24 @@
   (:gen-class)
   (:require [aoc-2022.utils :refer [read-resource]]))
 
+(def loses-to
+  {"A" "Z" ;; rock > scissors
+   "B" "X" ;; paper > rock
+   "C" "Y" ;; scissors > paper
+  })
+
+(def ties-to
+  {"A" "X" ;; rock == rock
+   "B" "Y" ;; paper == paper
+   "C" "Z" ;; scissors == scissors
+  })
+
+(def wins-to
+  {"A" "Y" ;; rock < paper
+   "B" "Z" ;; paper < scisors
+   "C" "X" ;; scissors < rock
+  })
+
 (defn score-round [[p1-choice p2-choice]]
   (+ (case p2-choice
        "X" 1
@@ -21,11 +39,22 @@
              "Y" 0
              "Z" 3))))
 
+(defn choose-p2-and-score-round [[p1-choice intended-outcome]]
+  (let [p2-choice (case intended-outcome
+                    "X" (loses-to p1-choice)
+                    "Y" (ties-to p1-choice)
+                    "Z" (wins-to p1-choice))]
+    (score-round [p1-choice p2-choice])))
+
 (defn day-02 []
  (let [strategy-guide (->> "day_02.txt"
                            read-resource
                            (map #(clojure.string/split % #" ")))
        part1 (->> strategy-guide
                   (map score-round)
+                  (apply +))
+       part2 (->> strategy-guide
+                  (map choose-p2-and-score-round)
                   (apply +))]
-   (do (println "Part 1:" part1))))
+   (do (println "Part 1:" part1)
+       (println "Part 2:" part2))))
